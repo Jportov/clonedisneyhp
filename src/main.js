f645bd7f01ddb4ff7aa603f69ac9e686b3b18901
victorportodev <victorportodev@gmail.com>
2024-04-15 20:51:46 -0300

    aaa

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (button) {
@@ -9,24 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
             escondeTodasAbas();
             aba.classList.add('show__list--is-active');
             removeBotaoAtivo();
-            button.target.classList.add('shows__tabs__button--is-active')
-        })
+            button.target.classList.add('shows__tabs__button--is-active');
+        });
     }
 });
 
-function removeBotaoAtivo () {
-    const buttons = document.querySelectorAll('[data-tab-button');
-    for (let i = 0; i < tabsContainer.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active ');
-        
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
     }
-};
+}
 
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
-            tabsContainer[i].classList.remove('shows__list--is-active');
-            
-        }
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
 }
