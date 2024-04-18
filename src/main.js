1add52753803ca093d12889a7eb2c9f74ae821d1
victorportodev <victorportodev@gmail.com>
2024-04-18 00:54:13 -0300

    3commit
    3 commit

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function (button) {
-            const abaAlvo = button.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+        buttons[i].addEventListener('click', function (event) {
+            const abaAlvo = event.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
             escondeTodasAbas();
-            aba.classList.add('show__list--is-active');
+            aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
-            button.target.classList.add('shows__tabs__button--is-active');
+            event.target.classList.add('shows__tabs__button--is-active');
+        });
+    }
+    
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', function(event) {
+            abreOuFechaResposta(event);
         });
     }
 });
+
+function abreOuFechaResposta(event) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = event.target.parentNode;
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -22,7 +35,6 @@ function removeBotaoAtivo() {
 
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
-
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
